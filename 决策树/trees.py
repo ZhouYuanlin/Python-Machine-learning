4d3fbb85d41de3f431f610ae9ee57b9510090108
木木同学 <mumutongxue@mumutongxuedeMacBook-Air.local>
2015-09-14 15:16:09 +0800

    trees

--- 决策树/trees.py
+++ 决策树/trees.py
@@ -1,19 +1,63 @@
-# -*-  utf-8 -*-
+# -*-  coding:utf-8 -*-
+
 #计算给定数据集的香农熵
+
+from numpy import *
 from math import log
 
 def calcShannonEnt(dataSet):
 	numEntries = len(dataSet)
-	laberCounts = {}
+	labelCounts = {}
 	#为所有可能分类创建字典
 	for featVec in dataSet:
 		currentLabel = featVec[-1]
-		if currentLabel not in laberCounts.key:
-			laberCounts[currentLabel] = 0
-		laberCounts[currentLabel] += 1
+		if currentLabel not in labelCounts.keys():
+			labelCounts[currentLabel] = 0
+		labelCounts[currentLabel] += 1
 	shannonEnt = 0.0
-	for key in laberCounts:
+	for key in labelCounts:
 		prob = float(labelCounts[key])/numEntries
 		#以2为底求对数
 		shannonEnt -= prob * log(prob, 2)
 	return shannonEnt
+
+#生成自己的数据集
+def createDataSet():
+	dataSet = [[1, 1, 'yes'],
+	           [1, 1, 'yes'],
+	           [1, 0, 'no'],
+	           [0, 1, 'no'],
+	           [0, 1, 'no']]
+	labels = ['no surfacing', 'flippers']
+	return dataSet, labels
+
+#按照给定特征划分数据集
+def splitDataSet(dataSet, axis, value):
+	retDataSet = []
+	for featVec in dataSet:
+		if featVec[axis] == value:
+			reducedFeatVec = featVec[:axis]
+			reducedFeatVec.extend(featVec[axis+1:])
+			retDataSet.append(reducedFeatVec)
+	return retDataSet
+
+#选择最好的数据集划分方式
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0]) - 1
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0
+	bestFeature  = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+		for value in uniqueVals:
+			subDataSet = splitDataSet(dataSet, i , value)
+			prob = len(subDataSet)/float(len(dataSet))
+			newEntropy += prob * calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntropy
+		if (infoGain > bestInfoGain):
+			bestInfoGain = infoGain
+			bestFeature = i
+		return bestFeature
+    
